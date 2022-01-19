a3346871f4ff1a66c395526a994aac531b4b5187
Lucas Lisboa <97049570+LucaszLisboa@users.noreply.github.com>
2022-01-19 02:14:05 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# cracha
+Cracha vitual criado na trilha origin na NLW Heat da Rocketseat
---
+++ main.js
@@ -0,0 +1,31 @@
+const linksSocialMedia = {
+  github: 'LucaszLisboa',
+  twitter: 'lucaszlisboa',
+  instagram: 'lucasz_lisboa',
+  linkedin: 'lucas-gabriel-lisboa-alves-a079a31a6'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
